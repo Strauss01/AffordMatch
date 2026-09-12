867b5609233911904c09242e93d3193b39ae4467
Levy Boipelo Sehalahala <sehalahala07@gmail.com>
2026-09-12 15:22:36 +0200

    Delete affordmatch-backend/affordmatch-backend directory

--- affordmatch-backend/affordmatch-backend/src/routes/public.routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const router = require("express").Router();
-const { asyncHandler } = require("../middleware/errorHandler");
-const ctrl = require("../controllers/listingsController");
-
-router.get("/listings", asyncHandler(ctrl.listPublicListings));
-
-module.exports = router;
